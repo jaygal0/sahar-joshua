dc34e6dbc7bc7f974376d5a2594db85ab6146063
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2022-09-08 20:37:52 +0200

    updated the index face

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50vw;
+  /* width: 50vw; */
   height: min-content;
   align-items: center;
   justify-content: center;
