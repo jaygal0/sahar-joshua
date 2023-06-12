9412876a193ba92cd1afe1e38d5416ebd1e6b970
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2023-06-12 21:27:01 +0200

    update contributions

--- data/contributions.js
+++ data/contributions.js
@@ -2,8 +2,15 @@ export default [
   {
     name: 'Leo & Andrea Galinato',
     amount: 6800,
-    timestamp: '2023-06-23',
+    timestamp: '2023-05-23',
     merchant: 'monzo',
+    anonymous: true,
+  },
+  {
+    name: 'Azar',
+    amount: 500,
+    timestamp: '2023-06-12',
+    merchant: 'swish',
     anonymous: true,
   },
 ]
